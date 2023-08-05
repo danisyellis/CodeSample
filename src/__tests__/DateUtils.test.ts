2893cf4e5c31cf00b6b6008ca5b93332a2b349cd
danisyellis <daniellegellis@gmail.com>
2023-08-04 22:57:43 -0700

    more clarity when running tests

--- src/__tests__/DateUtils.test.ts
+++ src/__tests__/DateUtils.test.ts
@@ -13,10 +13,14 @@ describe('formatting dates for mostViewed', () => {
 describe('calculating the correct ending date for a week or a month', () => {
     it('should return an ending date 6 days later if the duration = week', () => {
         expect(dateUtils.calculateEndingDate('week', '20230701')).toBe('20230707');
+    });
+    it('should work over when the week ends in the next month', () => {
         expect(dateUtils.calculateEndingDate('week', '20230726')).toBe('20230801');
     });
     it('should return an ending date that is 1 month away (minus 1 day because of how the wikipedia API uses dates), if the duration = month', () => {
         expect(dateUtils.calculateEndingDate('month', '20230701')).toBe('20230731');
+    });
+    it('should work over when the sliding month ends in the next calendar month', () => {
         expect(dateUtils.calculateEndingDate('month', '20230726')).toBe('20230825');
     });
     it('should return an error if the duration is anything besides week or month', () => {
